fef932335a993504a94a58a527658aa3d695a1b1
Christina Ach <Christina.Ach@TOLEDO.LCCC.LC3>
2017-02-14 14:08:12 -0500

    Added arithmetic operators

---
+++ arithmetic.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include "arithmetic.h"
+//#include "Stack.h"
+
+arithmetic::arithmetic()
+{
+}
+
+void arithmetic::add()
+{
+	// int x = Stack::pop();
+	// int y = Stack::pop();
+	// Stack::push((x+y));
+}
+
+void arithmetic::sub()
+{
+	// int x = Stack::pop();
+	// int y = Stack::pop();
+	// Stack::push((x-y));
+}
+
+void arithmetic::mult()
+{
+	// int x = Stack::pop();
+	// int y = Stack::pop();
+	// Stack::push((x*y));
+}
+
+void arithmetic::div()
+{
+	// int x = Stack::pop();
+	// int y = Stack::pop();
+	// Stack::push((x/y));
+}
+
+void arithmetic::mod()
+{
+	// int x = Stack::pop();
+	// int y = Stack::pop();
+	// Stack::push((x%y));
+}
+
+
+arithmetic::~arithmetic()
+{
+}
---
+++ arithmetic.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class arithmetic
+{
+public:
+	arithmetic();
+	void add();
+	void sub();
+	void mult();
+	void div();
+	void mod();
+	~arithmetic();
+};
