5858178b864aae51bc73495a57b861295621e2dc
Christina Ach <xshatteredreality@gmail.com>
2017-02-16 15:16:26 -0500

    Added Control Flow logic, goto is still a work in progress

---
+++ CTRL_FLOW.cpp
@@ -0,0 +1,66 @@
+// Control Flow Instructions
+
+#include "stdafx.h"
+#include "CTRL_FLOW.h"
+#include "stack_manipulation.h"
+
+CTRL_FLOW::CTRL_FLOW()
+{
+}
+
+void CTRL_FLOW::getLabel()
+{
+	//if (parameter.length() == 0)
+	//{
+	//	cout << "Need a parameter in order to specify which label to retrieve." << endl;
+	//	return;
+	//}
+
+}
+
+void CTRL_FLOW::goTo()
+{
+	// Not quite sure how to approach this
+}
+
+void CTRL_FLOW::goFalse()
+{
+	//if (stack.size() == 0)
+	//{
+	//	cout << "Stack is empty, therefore there is nothing to check" << endl;
+	//	return;
+	//}
+
+	//if (stack.top() == 0)
+	//{
+	//	getLabel();
+	//}
+
+	//stack.pop();
+}
+
+void CTRL_FLOW::goTrue()
+{
+	//if (stack.size() == 0)
+	//{
+	//	cout << "Stack is empty, therefore there is nothing to check" << endl;
+	//	return;
+	//}
+
+	//if (stack.top() == 1)
+	//{
+	//	getLabel();
+	//}
+
+	//stack.pop();
+}
+
+void CTRL_FLOW::halt()
+{
+	//return 0;
+}
+
+
+CTRL_FLOW::~CTRL_FLOW()
+{
+}
---
+++ CTR_FLOW.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class CTR_FLOW
+{
+public:
+	CTR_FLOW();
+	void getLabel();
+	void goTo();
+	void goFalse();
+	void goTrue();
+	void halt();
+	~CTR_FLOW();
+};
