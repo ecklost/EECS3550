e1d22d879da8f8d1b3923acad557407bd801477d
Christina Ach <xshatteredreality@gmail.com>
2017-02-18 14:28:01 -0500

    Removed unneeded comments

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main()
 {
-	vector<string> inputFileParameter;							// Vector that contains the input strings
+	vector<string> inputFileParameter;
 	
-	readInputFile(inputFileParameter);							// Calls the method that reads from the input file
+	readInputFile(inputFileParameter);
 	
 	// Wait for user to press 'Enter' to exit program
 	system("pause");
@@ -19,18 +19,18 @@ int main()
 
 void readInputFile(vector<string>& inputFileParameter)
 {
-	string singleLine = "";								// Stores each line in the input file as a string
+	string singleLine = "";
 	ifstream inputFile("input.txt");
 
 	cout << "\nThe following is what the input file contains: \n\n";
 
 	if (inputFile.is_open())
 	{
-		while (!inputFile.eof() && getline(inputFile, singleLine))	// While not at the end of the file, read file line by line
+		while (!inputFile.eof() && getline(inputFile, singleLine))
 		{
-			std::cout << singleLine << std::endl;		// Outputs every single line from the input File
+			std::cout << singleLine << std::endl;
 
-			if (singleLine.compare("halt") == 0)		// Checks if the file contains the following string: halt, if encountered, end the file
+			if (singleLine.compare("halt") == 0)
 				break;
 			else
 			{
